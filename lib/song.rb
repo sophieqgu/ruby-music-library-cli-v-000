539af96eed206740331634a8083751e43f6a47fe
Sophie Gu <sophieqgu@gmail.com>
2019-04-10 03:44:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,7 +53,6 @@ class Song
   end 
   
   def self.create_from_filename(filename)
-    song = self.new_from_filename(filename)
-    song.save
+    self.new_from_filename(filename).save
   end 
 end 
