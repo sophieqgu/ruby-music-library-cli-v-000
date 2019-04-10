896ab366d9347f5869d3b6b8abb1433dcd7cf3af
Sophie Gu <sophieqgu@gmail.com>
2019-04-10 04:06:20 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -37,11 +37,11 @@ class MusicLibraryController
   end
   
   def list_songs 
-    Song.all.sort_by(&:name).each.with_index(1) do |song, index|
+    Song.all.sort_by(&:name).each_with_index(1) do |song, index|
       puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
   end 
   
   def list_artists
-    Artist.all.sort_by(&:name).each.with_index
+    Artist.all.sort_by(&:name).each_with_index
 end 
