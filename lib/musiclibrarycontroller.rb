30e163957c8bb16dccadc94216b8311004a49ea5
Sophie Gu <sophieqgu@gmail.com>
2019-04-10 03:55:01 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -1,8 +1,11 @@
 class MusicLibraryController
+  attr_accessor :path 
   def initialize(path = './db/mp3s')
-    MusicImporter.new(path).import
+    @path = path 
+    MusicImporter.new(@path).import
   end 
   
   def call 
+    
   end 
 end 
